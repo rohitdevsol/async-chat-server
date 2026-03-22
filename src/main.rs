b21b0ede1058d2138184663f944ddba2eca15d84
Rohit <rohitnarnolia88@gmail.com>
2026-03-23 01:51:20 +0530

    add mpsc and tokio thread for handling multiple clients

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,42 @@
 use tokio::net::TcpListener;
 use tokio_tungstenite::accept_async;
 use futures::StreamExt;
-
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     println!("Server listening on the port 3000");
 
+    let (tx, mut rx) = mpsc::channel(100);
+
 
     loop {
         let (stream,_) = listener.accept().await.unwrap();
         println!("new connection ");
 
-        let mut ws_stream = accept_async(stream).await.unwrap();
-        println!("Websocket connected");
 
-        while let Some(message) = ws_stream.next().await{
-            let msg =message.unwrap();
-            println!("Received {:?}",msg.to_text().unwrap());
-        }
-        
+        // handle multiple clients
+        let tx_clone = tx.clone();
+        tokio::spawn(async move{
+            let mut ws_stream = accept_async(stream).await.unwrap();
+              println!("Websocket connected");
+
+            while let Some(message) = ws_stream.next().await{
+                let msg = message.unwrap().to_string();
+                // println!("Received {}",msg.to_text().unwrap());
+                tx_clone.send(msg).await.unwrap();
+            }
+
+        });
+
+              
 
     }
+
+    tokio::spawn(async move {
+        while let Some(event) = rx.recv().await {
+            println!("Server event received");
+        }
+    });
 }
